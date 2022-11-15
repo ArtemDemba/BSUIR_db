eb09b7f42372a7d84f4f137c20a507b13709881b
ArtemDemba <DVL.45@mail.ru>
2022-11-16 00:12:29 +0300

    Fixed FK's and added ON DELETE options

--- creates.sql
+++ creates.sql
@@ -19,17 +19,31 @@ CREATE TABLE IF NOT EXISTS suppliers
 );
 
 
+CREATE TABLE IF NOT EXISTS users
+(
+    user_id serial PRIMARY KEY,
+    first_name varchar(127) NOT NULL,
+    last_name varchar(127) NOT NULL,
+    phone varchar(13) UNIQUE NOT NULL,
+    email text
+);
+
+
+CREATE TABLE IF NOT EXISTS customers
+(
+    customer_id serial PRIMARY KEY,
+    user_id int UNIQUE NOT NULL REFERENCES users(user_id) ON DELETE CASCADE,
+    address text UNIQUE NOT NULL
+);
+
+
 CREATE TABLE IF NOT EXISTS orders
 (
     order_id serial PRIMARY KEY,
     create_date date NOT NULL,
     deliver_date date default NULL,
-    customer_id int NOT NULL
+    customer_id int NOT NULL REFERENCES customers(customer_id) ON DELETE CASCADE	
 );
-
-
-ALTER TABLE orders
-ADD CONSTRAINT FK_orders_customers FOREIGN KEY(customer_id) REFERENCES customers(customer_id);
 
 
 CREATE TABLE IF NOT EXISTS shops
@@ -46,28 +60,10 @@ CREATE TABLE IF NOT EXISTS books
     title text UNIQUE NOT NULL,
     production_date date NOT NULL,
     price decimal, 
-    order_id int REFERENCES orders(order_id) default NULL,
-    publisher_id int REFERENCES publishers(publisher_id) NOT NULL,
-    supplier_id int REFERENCES suppliers(supplier_id) NOT NULL,
-    shop_id int REFERENCES  shops(shop_id) NOT NULL
-);
-
-
-CREATE TABLE IF NOT EXISTS users
-(
-    user_id serial PRIMARY KEY,
-    first_name varchar(127) NOT NULL,
-    last_name varchar(127) NOT NULL,
-    phone varchar(13) UNIQUE NOT NULL,
-    email text
-);
-
-
-CREATE TABLE IF NOT EXISTS customers
-(
-    customer_id serial PRIMARY KEY,
-    user_id int REFERENCES users(user_id) UNIQUE NOT NULL,
-    address text UNIQUE NOT NULL
+    order_id int default NULL REFERENCES orders(order_id) ON DELETE RESTRICT,
+    publisher_id int NOT NULL REFERENCES publishers(publisher_id) ON DELETE SET NULL,
+    supplier_id int NOT NULL REFERENCES suppliers(supplier_id) ON DELETE SET NULL,
+    shop_id int NOT NULL REFERENCES  shops(shop_id) ON DELETE SET NULL
 );
 
 
@@ -81,19 +77,19 @@ CREATE TABLE IF NOT EXISTS positions
 CREATE TABLE IF NOT EXISTS employees
 (
     employee_id serial PRIMARY KEY,
-    user_id int REFERENCES users(user_id) UNIQUE NOT NULL,
+    user_id int UNIQUE NOT NULL REFERENCES users(user_id) ON DELETE CASCADE,
     birth_date date NOT NULL,
     address text NOT NULL,
     position_id int REFERENCES positions(position_id),
     salary decimal NOT NULL CHECK(salary >= 0),
-    shop_id int REFERENCES shops(shop_id) NOT NULL
+    shop_id int NOT NULL REFERENCES shops(shop_id) ON DELETE SET NULL
 );
 
 
 CREATE TABLE IF NOT EXISTS authors
 (
     author_id serial PRIMARY KEY,
-    user_id int REFERENCES users(user_id) UNIQUE NOT NULL,
+    user_id int UNIQUE NOT NULL REFERENCES users(user_id) ON DELETE CASCADE,
     author_alias varchar(255) default NULL
 );
 
@@ -107,36 +103,36 @@ CREATE TABLE IF NOT EXISTS genres
 
 CREATE TABLE IF NOT EXISTS book_genre
 (
-    book_id int REFERENCES books(book_id),
-    genre_id int REFERENCES genres(genre_id)
+    book_id int REFERENCES books(book_id) ON DELETE CASCADE,
+    genre_id int REFERENCES genres(genre_id) ON DELETE CASCADE
 );
 
 
 CREATE TABLE IF NOT EXISTS author_genre
 (
-    author_id int REFERENCES authors(author_id),
-    genre_id int REFERENCES genres(genre_id)
+    author_id int REFERENCES authors(author_id) ON DELETE CASCADE,
+    genre_id int REFERENCES genres(genre_id) ON DELETE CASCADE
 );
 
 
 CREATE TABLE IF NOT EXISTS book_author
 (
-    book_id int REFERENCES books(book_id),
-    author_id int REFERENCES authors(author_id)
+    book_id int REFERENCES books(book_id) ON DELETE CASCADE,
+    author_id int REFERENCES authors(author_id) ON DELETE CASCADE
 );
 
 
 CREATE TABLE IF NOT EXISTS order_book
 (
-    order_id int REFERENCES orders(order_id),
-    book_id int REFERENCES books(book_id)
+    order_id int REFERENCES orders(order_id) ON DELETE CASCADE,
+    book_id int REFERENCES books(book_id) ON DELETE CASCADE
 );
 
 
 CREATE TABLE IF NOT EXISTS book_shop
 (
-    shop_id int REFERENCES shops(shop_id),
-    book_id int REFERENCES books(book_id),
+    shop_id int REFERENCES shops(shop_id) ON DELETE CASCADE,
+    book_id int REFERENCES books(book_id) ON DELETE CASCADE,
     book_amount int CHECK(book_amount >= 0)
 );
 
@@ -145,5 +141,5 @@ CREATE TABLE IF NOT EXISTS logging
 (
     log_id serial PRIMARY KEY,
     log_message varchar(255) NOT NULL,
-    user_id int REFERENCES users(user_id)
+    user_id int REFERENCES users(user_id) ON DELETE SET NULL
 );
