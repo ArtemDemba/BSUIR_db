8e08f923d81f6cfff0bfc0e0122e32854d0309aa
ArtemDemba <80354610+ArtemDemba@users.noreply.github.com>
2022-12-02 00:41:21 +0300

    Держи, Димон

--- lab4.sql
+++ lab4.sql
@@ -9,7 +9,7 @@ HAVING COUNT(*) > 10
 ORDER BY COUNT(*);
 
 
-select order_id, SUM(price) from books
+SELECT order_id, SUM(price) FROM books
 GROUP BY order_id
 HAVING order_id IS NOT NULL
 ORDER BY SUM(price) DESC;
@@ -50,3 +50,54 @@ WHERE deliver_date IS NULL
 ORDER BY create_date;
 
 
+SELECT * FROM books
+LIMIT (SELECT COUNT(*) FROM customers);
+
+
+INSERT INTO genres(genre)
+SELECT first_name FROM users
+LIMIT 10;
+
+
+SELECT title, price,
+CASE WHEN price >= 90 THEN 'exspensive'
+	 WHEN price BETWEEN 50 and 90 THEN 'avarage'
+	 ELSE 'cheep'
+END as price_level
+FROM books
+ORDER BY price DESC;
+
+
+SELECT title, production_date,
+CASE WHEN date_part('years', production_date) > 1990 THEN 'new'
+	 ELSE 'old'
+END AS age_level, 
+price
+FROM books
+ORDER BY age_level;
+
+
+SELECT CASE WHEN date_part('years', production_date) > 1990 THEN 'new'
+			ELSE 'old'
+		END AS age_level,
+AVG(price)
+FROM books
+GROUP BY age_level;
+
+
+SELECT title, price, orders.create_date, orders.deliver_date FROM books
+LEFT JOIN orders USING(order_id)
+ORDER BY create_date;
+
+
+SELECT shops.shop_id, shops.address, COUNT(employees.user_id) FROM shops
+JOIN employees USING(shop_id)
+GROUP BY shops.address, shop_id
+ORDER BY shop_id;
+
+
+SELECT last_name, first_name, title FROM book_author
+JOIN books USING(book_id)
+JOIN authors USING(author_id)
+JOIN users USING(user_id)
+WHERE order_id=5;
